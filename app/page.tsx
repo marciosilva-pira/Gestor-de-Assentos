c3554a8c1e7cc6766fa93b60481b58fb30a4f696
Marcio Silva <marciosilva.pira@gmail.com>
2026-06-08 09:44:47 -0300

    08062026 e commit

--- app/page.tsx
+++ app/page.tsx
@@ -293,7 +293,7 @@ export default function Home() {
                   width: "100%",
                   height: "100%",
                   objectFit: "cover",
-                  display: "block",
+                  display: "visible",
                 }}
               />
             )}
