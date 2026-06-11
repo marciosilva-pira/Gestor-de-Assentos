42b3ad61d78306e8d4ee7a86b3bf981d71c97981
Marcio Silva <marciosilva.pira@gmail.com>
2026-06-11 10:04:05 -0300

    11062026 H commit

--- app/page.tsx
+++ app/page.tsx
@@ -430,7 +430,7 @@ export default function Home() {
               const timeout = setTimeout(() => {
                 setDragCadeira(cadeiraNum)
                 setIsDragging(true)
-              }, 200); // ⏱️ tempo de segurar (200ms)
+              }, 300); // ⏱️ tempo de segurar (200ms)
 
               setDragTimeout(timeout);
             }}
